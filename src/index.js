375715a376bbe21e16e3faeaf3cc8ff7a8736afd
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-27 01:58:18 -0300

    Feat: adding cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 //Routes
 app.use("/api", routes);
