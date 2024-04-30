f1aa2424a0d59819c041388d11389d2b6ce4b27c
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-30 00:30:47 -0300

    Feat: Commenting on a method that will not be used at the moment

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -24,6 +24,46 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// router.get("/tasks/:view", async (req, res) => {
+//   const { view } = req.params;
+//   let startDate, endDate;
+
+//   // Set start and end dates based on view option
+//   switch (view) {
+//     case "day":
+//       startDate = new Date();
+//       endDate = new Date();
+//       break;
+//     case "week":
+//       startDate = new Date();
+//       startDate.setDate(startDate.getDate() - startDate.getDay());
+//       endDate = new Date(startDate);
+//       endDate.setDate(endDate.getDate() + 6);
+//       break;
+//     case "month":
+//       startDate = new Date();
+//       startDate.setDate(1);
+//       endDate = new Date(startDate);
+//       endDate.setMonth(endDate.getMonth() + 1);
+//       endDate.setDate(endDate.getDate() - 1);
+//       break;
+//     default:
+//       return res.status(400).json({ error: "Invalid view option" });
+//   }
+
+//   try {
+//     // Query tasks based on start and end dates
+//     const tasks = await Task.find({
+//       deadline: { $gte: startDate, $lte: endDate },
+//     });
+
+//     res.json({ tasks });
+//   } catch (error) {
+//     console.error("Error when searching for tasks:", error);
+//     res.status(500).json({ error: "Error when searching for tasks" });
+//   }
+// });
+
 router.get("/count", async (req, res) => {
   try {
     const count = await Task.countDocuments();
@@ -69,5 +109,27 @@ router.put("/:id", async (req, res) => {
 
   return res.send(task);
 });
+
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const { description } = req.body;
+
+//     const updatedTask = await Task.findByIdAndUpdate(
+//       id,
+//       { description },
+//       { new: true }
+//     );
+
+//     if (!updatedTask) {
+//       return res.status(404).send("Task not found");
+//     }
+
+//     res.status(200).json(updatedTask);
+//   } catch (error) {
+//     console.error("Error updating description:", error);
+//     res.status(500).send("Internal server error");
+//   }
+// });
 
 module.exports = router;
